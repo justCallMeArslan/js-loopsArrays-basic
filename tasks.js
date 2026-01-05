26008d9a1b218ec588ebdef8e31e441aaee8250a
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-01-05 14:34:25 +0300

    Complete Array lesson in JS.info.

--- tasks.js
+++ tasks.js
@@ -89,11 +89,11 @@ while (j < 3) {
 // non-numeric input in this task.
 
 
-let input = +prompt("Input a number greater then 100", "0");
+// let input = +prompt("Input a number greater then 100", "0");
 
-while (input <= 100 && input) { // && input used for the cases where number more than 100 or null 
-    input = +prompt("Input a number greater then 100", "0");
-}
+// while (input <= 100 && input) { // && input used for the cases where number more than 100 or null 
+//     input = +prompt("Input a number greater then 100", "0");
+// }
 
 
 // Output prime numbers
@@ -116,4 +116,112 @@ for (let i = 2; i <= n; i++) {
     }
     console.log(i);
 }
+
+
+// What is this code going to show?
+
+let fruits = ["Apples", "Pear", "Orange"];
+
+// push a new value into the "copy"
+let shoppingCart = fruits;
+shoppingCart.push("Banana");
+
+// what's in fruits?
+console.log(fruits.length); // answer is 4, because array is an object and both variables are references to the
+//same array
+
+
+// Let’s try 5 array operations.
+
+// - Create an array styles with items “Jazz” and “Blues”.
+// - Append “Rock-n-Roll” to the end.
+// - Replace the value in the middle with “Classics”. Your code for finding the middle value should work for 
+// any arrays with odd length.
+// - Strip off the first value of the array and show it.
+// - Prepend Rap and Reggae to the array.
+
+// The array in the process:
+// Jazz, Blues
+// Jazz, Blues, Rock-n-Roll
+// Jazz, Classics, Rock-n-Roll
+// Classics, Rock-n-Roll
+// Rap, Reggae, Classics, Rock-n-Roll
+
+
+let styles = ["Jazz", "Blues"]; // Jazz, Blues
+
+styles.push("Rock-n-Roll"); //Jazz, Blues, Rock-n-Roll
+
+// styles[1] = "Classics"; // Jazz, Classics, Rock-n-Roll - suitable only in that case
+styles[Math.floor((styles.length - 1) / 2)] = "Classics"; // correct answer to task
+
+styles.shift(); // Classics, Rock-n-Roll
+
+styles.unshift("Rap", "Reggae"); // Rap, Reggae, Classics, Rock-n-Roll
+
+
+
+// What is the result? Why?
+
+let arr = ["a", "b"];
+
+arr.push(function () {
+    console.log(this);
+});
+
+arr[2](); // ["a", "b", function()] if this is passed; if we pass string or number, result is value we passed
+
+
+
+// Write the function sumInput() that:
+
+// - Asks the user for values using prompt and stores the values in the array.
+// - Finishes asking when the user enters a non-numeric value, an empty string, or presses “Cancel”.
+// - Calculates and returns the sum of array items.
+// P.S. A zero 0 is a valid number, please don’t stop the input on zero.
+
+function sumInput() {
+
+    let inputs = [];
+
+    while (true) {
+        let value = prompt("Enter your number", 0);
+
+        if (value === "" || value === null || !isFinite(value)) break; // checking for input; isFinite needed 
+        // to check if input is anything but number or string with numbers
+
+        inputs.push(+value); // + to convert string to number in array
+    }
+
+    let sum = 0;
+    for (input of inputs) { // for each input add it to sum and store value
+        sum += input;
+    }
+    return sum;
+}
+
+console.log(sumInput());
+
+
+
+// A maximal subarray - UNSOLVED!
+
+// The input is an array of numbers, e.g. arr = [1, -2, 3, 4, -9, 6].
+// The task is: find the contiguous subarray of arr with the maximal sum of items.
+// Write the function getMaxSubSum(arr) that will return that sum.
+
+// For instance:
+
+// getMaxSubSum([-1, 2, 3, -9]) == 5 (the sum of highlighted items)
+// getMaxSubSum([2, -1, 2, 3, -9]) == 6
+// getMaxSubSum([-1, 2, 3, -9, 11]) == 11
+// getMaxSubSum([-2, -1, 1, 2]) == 3
+// getMaxSubSum([100, -9, 2, -3, 5]) == 100
+// getMaxSubSum([1, 2, 3]) == 6 (take all)
+
+// If all items are negative, it means that we take none (the subarray is empty), so the sum is zero:
+
+// getMaxSubSum([-1, -2, -3]) = 0
+
+// Please try to think of a fast solution: O(n2) or even O(n) if you can.
 
